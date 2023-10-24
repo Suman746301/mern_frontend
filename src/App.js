692e491a26f5c573d758062174104829c3735800
Suman746301 <74864276+Suman746301@users.noreply.github.com>
2023-10-25 00:12:01 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Home from "./screens/Home";
+import Signup from "./screens/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css"; //npm i bootstrap-dark-5 boostrap
+
+import MyOrder from "./screens/MyOrder";
+import { CartProvider } from "./components/ContextReducer";
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Login" element={<Login />} />
+            <Route exact path="/Signup" element={<Signup />} />
+            <Route exact path="/myOrder" element={<MyOrder />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,142 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useDispatchCart, useCart } from "./ContextReducer";
+import { useNavigate } from "react-router-dom";
+
+export default function Card(props) {
+  let dispatch = useDispatchCart();
+  let data = useCart();
+  let options = props.options;
+  let priceOptions = Object.keys(options);
+  let navigate = useNavigate();
+  let [qty, setQty] = useState(1);
+  let [size, setSize] = useState("");
+  const priceRef = useRef();
+  const [btnEnable, setBtnEnable] = useState(false);
+
+  // const handleClick = () => {
+  //   if (!localStorage.getItem("token")) {
+  //     navigate("/myOrder");
+  //   }
+  // };
+  const handleQty = (e) => {
+    setQty(e.target.value);
+  };
+  const handleOptions = (e) => {
+    setSize(e.target.value);
+  };
+
+  const handleAddToCart = async () => {
+    let food = [];
+    for (const item of data) {
+      if (item.id === props.foodItem._id) {
+        food = item;
+        break;
+      }
+    }
+
+    //If only the qty of the order changes
+    //And change for only when size
+    if (food !== []) {
+      if (food.size === size) {
+        await dispatch({
+          type: "UPDATE",
+          id: props.foodItem._id,
+          price: finalPrice,
+          qty: qty,
+        });
+        return;
+      } else if (food.size !== size) {
+        await dispatch({
+          type: "ADD",
+          id: props.foodItem._id,
+          name: props.foodItem.name,
+          price: finalPrice,
+          qty: qty,
+          size: size,
+        });
+        return;
+      }
+      return;
+    }
+    await dispatch({
+      type: "ADD",
+      id: props.foodItem._id,
+      name: props.foodItem.name,
+      price: finalPrice,
+      qty: qty,
+      size: size,
+    });
+    //console.log(data);
+  };
+
+  let finalPrice = qty * parseInt(options[size]);
+
+  useEffect(() => {
+    setSize(priceRef.current.value);
+  }, []);
+
+  return (
+    <div>
+      <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
+        <img
+          src={props.foodItem.img}
+          className="card-img-top"
+          alt="..."
+          style={{ height: "120px", objectFit: "fill" }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.foodItem.name}</h5>
+          <div className="container w-100 p-0" style={{ height: "38px" }}>
+            <select
+              className="m-2 h-100 w-20 bg-success text-black rounded"
+              style={{ select: "#FF0000" }}
+              // onClick={handleClick}
+              onChange={handleQty}
+            >
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              className="m-2 h-100 w-20 bg-success text-black rounded"
+              style={{ select: "#FF0000" }}
+              ref={priceRef}
+              // onClick={handleClick}
+              onChange={handleOptions}
+            >
+              {priceOptions.map((i) => {
+                return (
+                  <option key={i} value={i}>
+                    {i}
+                  </option>
+                );
+              })}
+            </select>
+            <div className=" d-inline ms-2 h-100 w-20 fs-5">
+              ₹{finalPrice}/-
+            </div>
+          </div>
+          <hr></hr>
+          <button
+            className={`btn btn-success justify-center ms-2 `}
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </button>
+          {/* <button
+            className={`btn btn-danger justify-center ms-2 ${
+              btnEnable ? "" : "disabled"
+            }`}
+            onClick={handleRemoveCart}
+          >
+            Remove
+          </button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
